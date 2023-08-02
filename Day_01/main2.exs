e6c20e47cdfa295e38bc6c437a894bfbeb51eeac
francesco <francesco.protopapa22@gmail.com>
2023-08-02 22:29:24 +0200

    fix day 1 to 7

---
+++ Day_01/main2.exs
@@ -0,0 +1,9 @@
+[a, b, c | _] = File.read!("../input.txt") 
+    |> String.split("\n\n") 
+    |> Enum.map(fn x -> String.split(x, "\n") end) 
+    |> Enum.map(fn x -> Enum.map(x, fn y -> String.to_integer(y) end) end) 
+    |> Enum.map(fn x -> Enum.sum(x) end) 
+    |> Enum.sort()
+    |> Enum.reverse()
+
+IO.puts a + b + c
--- Day_02/main.r
+++
@@ -1,6 +0,0 @@
-sayHello <- function(){
-   my_data <- read.delim("input.txt", header = FALSE, sep = " ")
-   print(my_data[2][1])
-}
-
-sayHello()
